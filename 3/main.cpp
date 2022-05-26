8f5060f09bc57f3e0be2a1fc8282df86e4d6d83d
Igor Vereshchagin <igor.a.vereshchagin@gmail.com>
2022-05-26 09:39:41 +0300

    cpp_38.5.3 done

--- 3/main.cpp
+++ 3/main.cpp
@@ -2,9 +2,11 @@
 
 #include "./ui_blur.h"
 
+#include <QGraphicsPixmapItem>
 #include <QGraphicsScene>
 #include <QGraphicsBlurEffect>
 #include <QFileDialog>
+#include <QImage>
 #include <QPixmap>
 #include <QPaintEvent>
 #include <QPainter>
@@ -15,30 +17,49 @@ class BlurMainWindow : public QMainWindow
   Q_OBJECT
   QWidget* pictureView = nullptr;
   QSlider* blurSlider = nullptr;
-  QPixmap inputPicture;
-  QPixmap bluredPicture;
+  QImage sourceImage;
+  QImage bluredImage;
 public:
   BlurMainWindow(QWidget* parent = nullptr) : QMainWindow(parent) {}
   void paintEvent(QPaintEvent* e) override {
     assert(pictureView != nullptr);
     QPainter p(this);
-    p.drawPixmap(pictureView->rect(), bluredPicture);
+    p.drawPixmap(pictureView->rect(), QPixmap::fromImage(bluredImage));
   }
   void setPictureView(QWidget* widget) {pictureView = widget;}
   void setBlurSlider(QSlider* slider) {blurSlider = slider;}
 public slots:
-  void openFile() {
+  void openFileSlot() {
     assert(pictureView != nullptr);
     assert(blurSlider != nullptr);
-    inputPicture = QPixmap(QFileDialog::getOpenFileName(nullptr, "Open picture", "./../" ));
-    this->setFixedSize(inputPicture.rect().width(), inputPicture.rect().height() + 200);
-    pictureView->setGeometry(inputPicture.rect());
-    bluredPicture = inputPicture;
+    sourceImage = QImage(QFileDialog::getOpenFileName(nullptr, "Open picture", "./../" ));
+    this->setFixedSize(sourceImage.rect().width(), sourceImage.rect().height() + 200);
+    pictureView->setGeometry(sourceImage.rect());
+    bluredImage = sourceImage;
     blurSlider->setValue(0);
   };
-  void blurPicture(int val)
+  void blurImageSlot(int val)
   {
-    
+    bluredImage = blurImage(sourceImage, val);
+    update();
+  }
+  static QImage blurImage(const QImage &source, int radius)
+  {
+    if (source.isNull())
+      return QImage();
+    QGraphicsScene scene;
+    QGraphicsPixmapItem item;
+    item.setPixmap(QPixmap::fromImage(source));
+    auto *blur = new QGraphicsBlurEffect;
+    blur->setBlurRadius(radius);
+    item.setGraphicsEffect(blur);
+    scene.addItem(&item);
+    QImage result(source.size(), QImage::Format_ARGB32);
+    result.fill(Qt::transparent);
+    QPainter painter(&result);
+    scene.render(&painter, QRectF(), QRectF(0, 0, source.width(), source.height()));
+    delete blur;
+    return result;
   }
 };
 
@@ -50,8 +71,8 @@ int main(int argc, char *argv[])
   blur.setupUi(&window);
   window.setPictureView(blur.widget);
   window.setBlurSlider(blur.horizontalSlider);
-  QObject::connect(blur.pushButton, &QPushButton::clicked, &window, &BlurMainWindow::openFile);
-  QObject::connect(blur.horizontalSlider, &QSlider::valueChanged, &window, &BlurMainWindow::blurPicture);
+  QObject::connect(blur.pushButton, &QPushButton::clicked, &window, &BlurMainWindow::openFileSlot);
+  QObject::connect(blur.horizontalSlider, &QSlider::valueChanged, &window, &BlurMainWindow::blurImageSlot);
   window.resize(800, 600);
   window.show();
   return QApplication::exec();
